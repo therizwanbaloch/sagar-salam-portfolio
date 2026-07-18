ba22ea4c5c9310b444baee82b549b5ab913aedc9
Rizwan Baloch <comraderizwanbaloch@gmail.com>
2026-07-18 20:45:01 +0500

    hero added

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero'; 
+import Hero from "./Components/Hero" 
 import SummaryAndSkills from  "./Components/SummaryAndSkills";
 import Experience from "./Components/Experience"
 import Projects from "./Components/Projects"
