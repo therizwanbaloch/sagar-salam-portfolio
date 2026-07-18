79e7c39f1fb8989ceed46e690e1ab802cd03daea
Rizwan Baloch <comraderizwanbaloch@gmail.com>
2026-07-18 20:46:48 +0500

    navbar added

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero" 
 import SummaryAndSkills from  "./Components/SummaryAndSkills";
 import Experience from "./Components/Experience"
